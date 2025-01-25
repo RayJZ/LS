24b736400dee3a5ec08ab44b60c832d331449c93
RayJZ <oliverdoig@gmail.com>
2025-01-24 17:19:14 -0700

    Update setup.sh
    organization, error handling

--- setup.sh
+++ setup.sh
@@ -1,36 +1,59 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root." >&2
-    exit 1
-fi
-
 USERNAME="lnsw"
+UID=900
 # Marble LS pub key
 SSH_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIhejUPFR+fFUPFs3pulV0QNd1u2+Z5U769aWDVwsEcX nsoadmin@marble"
 AUTHORIZED_KEYS_DIR="/home/$USERNAME/.ssh"
 AUTHORIZED_KEYS_FILE="$AUTHORIZED_KEYS_DIR/authorized_keys"
 SUDOERS_FILE="/etc/sudoers.d/$USERNAME"
 
+# Test for root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root." >&2
+    exit 1
+fi
+
+# Test for /etc/sudoers.d
+if ! test -d "/etc/sudoers.d"; then
+    echo "Directory \"/etc/sudoers.d/\" does not exist, exiting." >&2
+    exit 1
+fi
+
+# Test to see if sudo file for user has already been created
+if test -d SUDOERS_FILE; then
+    echo "File \"$SUDOERS_FILE\" already exists, exiting." >&2
+    exit 1
+fi
+
+# Test to see if user already exists
+if id $USERNAME >/dev/null 2>&1; then
+    echo "User \"$USERNAME\" already exists, exiting." >&2
+    exit 1
+fi
+
+# Test if user's home directory already exists
+if test -d /home/$USERNAME; then
+    echo "Directory /home/\"$USERNAME\" already exists, exiting." >&2
+    exit 1
+fi
+
+# Test if UID is in already use.
+if id $UID >/dev/null 2>&1; then
+    echo "Default UID $UID is in use. Exiting." >&2
+    exit 1
+fi
+
+# Test if sshd is running.
+if ! pgrep -x "sshd" > /dev/null; then
+  echo "Process sshd is not running. Exiting." >&2
+  exit 1
+fi
+
 setup_user()
 {
-    if id $USERNAME >/dev/null 2>&1; then
-        echo "User \"$USERNAME\" already exists, exiting." >&2
-        exit 1
-    fi
-
-    if test -d /home/$USERNAME; then
-        echo "Directory /home/\"$USERNAME\" already exists, exiting." >&2
-        exit 1
-    fi
-
-    if id "900" >/dev/null 2>&1; then
-        echo "Default UID 900 is in use. Exiting." >&2
-        exit 1
-    fi
-
-    # Create system user with home directory. Don't know if it needs to be bash but bash definitely works.
-    useradd -r -m -u 900 -s /bin/bash "$USERNAME"
+    # Create system user with home directory.
+    useradd -r -m -u $UID -s /bin/bash "$USERNAME"
     # Create/set permissions for .ssh & .ssh/authorized_keys
     mkdir -p "$AUTHORIZED_KEYS_DIR"
     chmod 700 "$AUTHORIZED_KEYS_DIR"
@@ -45,25 +68,15 @@ setup_user()
     passwd -l "$USERNAME"
 }
 
-# Give sudo for least privilege commands
+# Give passwordless sudo for least privilege commands
 setup_sudo()
 {
-    if ! test -d "/etc/sudoers.d"; then
-        echo "Directory \"/etc/sudoers.d/\" does not exist, exiting." >&2
-        exit 1
-    fi
-
-    if test -d SUDOERS_FILE; then
-        echo "File \"$SUDOERS_FILE\" already exists, exiting." >&2
-        exit 1
-    fi
-
     cat > "$SUDOERS_FILE" << EOF
 $USERNAME ALL=(ALL) NOPASSWD: /usr/bin/echo, /usr/bin/lspci, /usr/sbin/dmidecode, /usr/sbin/smbios, /usr/sbin/lvs, /usr/sbin/pvs, /usr/sbin/vgs, /usr/sbin/ifconfig, /usr/sbin/lshw
 EOF
     chmod 440 "$SUDOERS_FILE"
 }
 
-echo "User lnsw setup successfully."
 setup_user
 setup_sudo
+echo "User \"$USERNAME\" setup successfully."
